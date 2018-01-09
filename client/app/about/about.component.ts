53b34985d4b8c8d380c7c4b325776f0d9b60a8cc
Vaibhav Mali <vm207@enovate-it.com>
2018-01-09 12:51:44 +0530

    Added timer for task in home

--- client/app/about/about.component.ts
+++ client/app/about/about.component.ts
@@ -1,9 +1,14 @@
-import { Component ,OnInit} from '@angular/core';
+import { Component, OnInit , ViewChild } from '@angular/core';
 import { ToastComponent } from '../shared/toast/toast.component';
 import { HomeService } from '../services/home.service';
 import { ProjectService } from '../services/project.service';
+import { TaskService } from '../services/task.service';
+import {Popup} from 'ng2-opd-popup';
+
+
 
 import { AuthService } from '../services/auth.service';
+import { Observable, Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-about',
@@ -12,6 +17,36 @@ import { AuthService } from '../services/auth.service';
 
 })
 export class AboutComponent implements OnInit{
+
+  //Vaibhav Mali 09 Jan 2018 ...Start
+
+  @ViewChild('popup3') popup3: Popup;  
+  timer;
+  ticks = 0;
+  init = 0;
+  selectvalue = 0;
+  task_id = "";
+  paused = 0;  
+  checkCommnetAssign=null;  
+  selectedTaskDetail={};  
+  hoursDisplay: number = localStorage.getItem("hoursDisplay") ? parseInt(localStorage.getItem("hoursDisplay")) : 0;    
+  minutesDisplay: number = localStorage.getItem("minutesDisplay") ? parseInt(localStorage.getItem("minutesDisplay")) : 0;    
+  secondsDisplay: number = localStorage.getItem("secondsDisplay") ? parseInt(localStorage.getItem("secondsDisplay")) : 0;    
+  AssignedtaskFormDetail = {
+    _id:"",
+    select:0,
+    status:0,
+    spendtime : {}
+ };
+ spendtime = {
+    start_date_time : new Date(),
+    end_date_time : new Date(),
+    actual_hrs : 0,
+    comment : ""
+  }
+  comment_data="";
+  
+  //Vaibhav Mali 09 Jan 2018 ...End
 
 
   isShowproject=true;
@@ -37,7 +72,8 @@ export class AboutComponent implements OnInit{
   constructor(public toast: ToastComponent,
   private homeService:HomeService,
   private auth:AuthService,
-  private projectService:ProjectService) { }
+  private projectService:ProjectService,
+  private taskService : TaskService) { }
   task_pagination={
     page:1,
     itemsPerPage:10,
@@ -46,12 +82,184 @@ export class AboutComponent implements OnInit{
     length:0
   }
   ngOnInit() {
-
+    this.setTimerValue();    
     this.auth.getLogedinUserData();
     setTimeout(()=>{    
       this.getProjectDetail();
     },50);
+
+       //Vaibhav Mali 09 Jan 2018 ...Start
+        this.selectvalue = localStorage.getItem("select") ? parseInt( localStorage.getItem("select")) : 0;      
+        if(parseInt(this.selectvalue.toString()) == 1){
+        this.AssignedtaskFormDetail._id = localStorage.getItem("task_id").toString();
+        this.AssignedtaskFormDetail.select = 1;
+        this.ticks = this.secondsDisplay;
+        this.setTimerValue()
+        this.startTimer(0);
+      }
+      //Vaibhav Mali 09 Jan 2018 ...End
   }
+
+  //Vaibhav Mali 09 Jan 2018 ...Start
+    /*
+  @author : Vaibhav Mali 
+  @date : 09 Jan 2018
+  @fn : startTimer,UpdateAssignedTaskDetail,startTaskFun,
+        pauseTaskFun,finishTaskFun,setTimerValue,showGetComment,conformGetComent,cancelGetComent.
+  */
+  showGetComment(task_detail){
+    this.selectedTaskDetail=task_detail;
+    this.popup3.show();
+  }
+  conformGetComent(){
+    if(this.checkCommnetAssign=="PAUSE"){
+      this.pauseTaskFun(this.selectedTaskDetail);
+    }else if(this.checkCommnetAssign=="FINISH"){
+      this.finishTaskFun(this.selectedTaskDetail);
+    }
+    this.popup3.hide();
+  }
+  cancelGetComent(){
+    this.popup3.hide();
+  }
+
+  setTimerValue() { 
+    var me =this; 
+    this.ticks = this.secondsDisplay ? this.secondsDisplay :0;  
+    this.hoursDisplay = localStorage.getItem("hoursDisplay") ? parseInt(localStorage.getItem("hoursDisplay")) : 0;      
+    this.minutesDisplay = localStorage.getItem("minutesDisplay") ? parseInt(localStorage.getItem("minutesDisplay")) : 0;    
+    this.secondsDisplay = localStorage.getItem("secondsDisplay") ? parseInt(localStorage.getItem("secondsDisplay")) : 0;    
+    setTimeout(function() {
+      me.setTimerValue()
+    }, 60*60*0.3);
+  }
+  private startTimer(value) {
+    if(value != 1){
+        var me = this;
+         var hoursDisplay,secondsDisplay,minutesDisplay;
+           secondsDisplay =   parseInt(this.secondsDisplay.toString()) + 1;
+           hoursDisplay = this.hoursDisplay;
+           minutesDisplay = this.minutesDisplay;
+          if(this.secondsDisplay == 60){
+            minutesDisplay =  parseInt(this.minutesDisplay.toString()) + 1; 
+            secondsDisplay = 0                  
+          }
+          if(this.minutesDisplay == 60){
+             hoursDisplay = parseInt(this.hoursDisplay.toString()) + 1;  
+            minutesDisplay =  0;                 
+   
+          }   
+         localStorage.setItem("hoursDisplay",(hoursDisplay).toString());
+         localStorage.setItem("minutesDisplay",(minutesDisplay).toString()); 
+         localStorage.setItem("secondsDisplay",(secondsDisplay).toString());               
+         localStorage.setItem("actual_hrs",(this.hoursDisplay + ((parseInt((this.minutesDisplay * 60).toString()) + parseInt(this.secondsDisplay.toString()))/3600)).toString());
+       
+          this.timer =  setTimeout(function() {
+            if(value == 1){
+               clearTimeout(this.timer);   
+            } 
+            else{
+              me.startTimer(0) 
+            }    
+          }, 60*60*0.2);
+        }
+        else{
+          clearTimeout(this.timer);                   
+        }
+}
+
+ 
+UpdateAssignedTaskDetail(){
+  console.log(this.AssignedtaskFormDetail);
+  this.taskService.saveTaskDetail({reqData:[this.AssignedtaskFormDetail]}).subscribe(
+    res => {
+        if(res.success.successData.length!=0){
+         this.comment_data="";
+         this.checkCommnetAssign="";
+         this.openAllTasks();
+        }
+    },
+    error => this.toast.setMessage('Some thing wrong!', 'danger')
+  );
+}
+startTaskFun(task){
+  this.AssignedtaskFormDetail._id = task._id;
+  this.AssignedtaskFormDetail.status = 1;
+  this.AssignedtaskFormDetail.select = 1;
+  this.paused = 0;  
+      if(task && task.spendtimes.length > 0){
+        delete this.AssignedtaskFormDetail['start_date_time'];
+      }
+      else{
+        this.AssignedtaskFormDetail['start_date_time'] = new Date();             
+      }
+      this.startTimer(0);      
+      delete this.AssignedtaskFormDetail['end_date_time'];
+      this.AssignedtaskFormDetail.spendtime = {};
+      this.spendtime['start_date_time'] = new Date();
+      localStorage.setItem('select', (1).toString());
+      localStorage.setItem('task_id',task._id.toString());      
+      localStorage.setItem('spend_start_date_time', this.spendtime['start_date_time'].toDateString());      
+      this.UpdateAssignedTaskDetail();
+}
+pauseTaskFun(task){
+  var me =this;
+  if (this.comment_data == null || this.comment_data == "" || this.comment_data ==undefined) {
+    this.toast.setMessage('Comment should not be blank!', 'danger');
+    return ;
+  }else{
+    this.AssignedtaskFormDetail._id = task._id;
+    this.AssignedtaskFormDetail.status = 1;
+    this.AssignedtaskFormDetail.select = 0;  
+    this.paused = 1;
+    delete this.AssignedtaskFormDetail['start_date_time'];
+    localStorage.setItem('spend_start_date_time', "");        
+    this.spendtime['end_date_time'] = new Date();
+    this.spendtime['comment'] = this.comment_data;  
+    localStorage.setItem('select', null);  
+    localStorage.setItem("hoursDisplay",(0).toString());
+    localStorage.setItem("minutesDisplay",(0).toString()); 
+    localStorage.setItem("secondsDisplay",(0).toString());             
+    me.startTimer(1)
+    this.spendtime.actual_hrs = this.hoursDisplay + ((parseInt((this.minutesDisplay * 60).toString()) + parseInt(this.secondsDisplay.toString()))/3600);
+    this.hoursDisplay = 0;            
+    this.minutesDisplay = 0;               
+    this.secondsDisplay = 0;  
+    this.AssignedtaskFormDetail.spendtime = this.spendtime;
+    this.UpdateAssignedTaskDetail();  
+  }
+}
+finishTaskFun(task){
+  if (this.comment_data == null || this.comment_data == "" || this.comment_data ==undefined) {
+    this.toast.setMessage('Comment should not be blank!', 'danger');
+    return ;
+  }else{
+    this.AssignedtaskFormDetail._id = task._id;
+    this.AssignedtaskFormDetail.status = 2;
+    this.AssignedtaskFormDetail.select = 0;  
+    delete this.AssignedtaskFormDetail['start_date_time'];
+    this.AssignedtaskFormDetail['end_date_time'] = new Date(); 
+    this.spendtime['end_date_time'] = new Date();
+    this.spendtime['comment'] = this.comment_data;    
+    localStorage.setItem('select', null);  
+    localStorage.setItem("hoursDisplay",(0).toString());
+    localStorage.setItem("minutesDisplay",(0).toString()); 
+    localStorage.setItem("secondsDisplay",(0).toString());       
+    this.startTimer(1)  
+    this.spendtime.actual_hrs = this.hoursDisplay + ((parseInt((this.minutesDisplay * 60).toString()) + parseInt(this.secondsDisplay.toString()))/3600);
+    if(this.paused == 1)
+      this.AssignedtaskFormDetail.spendtime = {};
+    else
+      this.AssignedtaskFormDetail.spendtime = this.spendtime;      
+    this.paused = 0;
+    this.hoursDisplay = 0;            
+    this.minutesDisplay = 0;               
+    this.secondsDisplay = 0;  
+    this.UpdateAssignedTaskDetail();
+  }
+}
+ //Vaibhav Mali 09 Jan 2018 ...End
+
   openProjectListDialog(){
     this.isShowproject=true;
     this.isShowList=false;
